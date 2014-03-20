1c734c96ecd8f40b4e414c62a12d8540ee215cc7
Kristie Starink <kristie@declarativ.com>
2014-03-20 15:32:24 +0000

    Add component flow

---
+++ ui/flow.reel/flow.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/flow.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Flow
+ * @extends Component
+ */
+exports.Flow = Component.specialize(/** @lends Flow# */ {
+    constructor: {
+        value: function Flow() {
+            this.super();
+        }
+    }
+});
